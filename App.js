c2047d8990514c69ae628e359a9b195163a82580
Owen Xue <owensit98@gmail.com>
2021-12-05 14:17:32 -0500

    add View, Text, and Styles Components

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Hello World!</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={{ fontSize: 30 }}>
+          Be the change that you wish to see in the world.
+        </Text>
+        <Text>Be the change that you wish to see in the world.</Text>
+        <Text>Be the change that you wish to see in the world.</Text>
+      </View>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -14,8 +23,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    backgroundColor: "pink",
+    padding: 20,
+  },
+  boldText: {
+    fontWeight: "bold",
+  },
+  body: {
+    backgroundColor: "yellow",
+    padding: 20,
   },
 });
